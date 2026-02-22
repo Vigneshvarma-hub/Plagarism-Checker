0789b963fda85e8ad54814d1dc24f819c1098112
Vignesh Varma <vigneshvarma353@gmail.com>
2026-02-22 07:34:24 +0530

    Added basic plagiarism comparison logic

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello GitHub")
+def check_plagiarism(text1, text2):
+    if text1 == text2:
+        return "100% Plagiarism"
+    else:
+        return "Not identical"
+
+print(check_plagiarism("hello", "hello"))
